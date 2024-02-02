89da2f56fa28915bbf3f4cd9e7cf84f30b07b168
DES2048 <electrochange@mail.ru>
2024-02-02 20:00:46 +0300

    update vite add vuetify support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import 'vite/modulepreload-polyfill';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+
 import Vue3TouchEvents from "vue3-touch-events";
 
 
@@ -8,11 +10,13 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import 'vuetify/styles'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Vue3TouchEvents);
+app.use(createVuetify())
 
 app.mount('#app')
